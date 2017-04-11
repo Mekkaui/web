2d641a587b38a54f38e0dfe2072ce8d22be0134f
Ismael Kaka <imohamadouk@enseirb-matmeca.fr>
2017-04-11 11:48:33 +0200

    First Commit Ismael MK

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Database configuration
+$GLOBALS['dbServ'] = 'localhost';
+$GLOBALS['dbUser'] = 'root';
+$GLOBALS['dbPass'] = '';
+$GLOBALS['dbName'] = 'petitions';
+
+// Blog configuration
+$GLOBALS['blogTitle'] = 'Bloggy';
+$GLOBALS['blogPass'] = 'secret';
+
+?>
